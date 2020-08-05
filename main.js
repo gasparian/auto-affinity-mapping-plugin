2d801d8d35a65b6a5cf42aa51241381b20f9aaf9
gas <gasparian>
2020-08-06 00:31:15 +0500

    debug with server

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ class WidgetsProcessor {
     process() {
         this.getWidgetClass().then((result) => {
             const data = result ? JSON.parse(result) : {}
-            if (data.length) {
+            if (Object.keys(data).length) {
                 this.updateWidgetsPos(data)
             }
         })
@@ -75,7 +75,7 @@ class WidgetsProcessor {
 
     updateWidgetsPos(widgetClass) {
         console.log(widgetClass)
-        
+
         Object.keys(widgetClass)
             .map(function(k) { return { key: k, value: languages[k] }; })
             .sort(function(a, b) { return b.value.length - a.value.length; });
